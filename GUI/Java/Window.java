c840fa5917e9f83a7e3e7507a32699fdd92826b3
Josegrowl <josegrowl@gmail.com>
2018-11-06 13:04:13 -0600

    added a simple JFrame

--- GUI/Java/Window.java
+++ GUI/Java/Window.java
@@ -1,6 +1,15 @@
+import java.awt.GraphicsConfiguration;
+import javax.swing.JFrame;
+
 public class Window{
+
+	static GraphicsConfiguration gc;
+
 	public static void main(String[] args){
-		System.out.println("Hello");
+		JFrame frame = new JFrame(gc);
+		frame.setTitle("RombaCop");
+		frame.setSize(600, 400);
+		frame.setVisible(true);
 	}
 
 
