e1797df0d6bb59112482726966a33c27105e54c4
Josegrowl <josegrowl@gmail.com>
2018-11-06 13:40:38 -0600

    added some btns for directions

--- GUI/Java/Window.java
+++ GUI/Java/Window.java
@@ -1,16 +1,56 @@
-import java.awt.GraphicsConfiguration;
-import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Window{
 
 	static GraphicsConfiguration gc;
+	static JButton btnFORWARDS;
+	static JButton btnBACKWARDS;
+	static JButton btnLEFT;
+	static JButton btnRIGHT;
+
 
 	public static void main(String[] args){
+		createWindow();	
+	}
+
+	private static void createWindow(){
 		JFrame frame = new JFrame(gc);
 		frame.setTitle("RombaCop");
 		frame.setSize(600, 400);
+
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent){
+				System.exit(0);
+			}
+		});
+		frame.add(createPanel());
+
 		frame.setVisible(true);
 	}
 
+	private static JPanel createPanel(){
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.darkGray);
+		panel.setSize(500, 300);
+		panel.setLayout(new FlowLayout());
+
+		createBtns();
+
+		panel.add(btnFORWARDS);
+		panel.add(btnBACKWARDS);
+		panel.add(btnLEFT);
+		panel.add(btnRIGHT);
+
+		return panel;
+	}
+
+	private static void createBtns(){
+		btnFORWARDS= new JButton("FORWARDS");
+		btnBACKWARDS= new JButton("BACKWARDS");
+		btnLEFT = new JButton("LEFT");
+		btnRIGHT =  new JButton("RIGHT");
+	}
 
 }
